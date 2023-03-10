441f83ed0a856340f9ebdb62e8e51517852aced7
hitesh <hiteshr@zignuts.com>
2023-03-10 11:25:15 +0530

    method index rename to list and removed searchArticle, sortArticle method

--- pagination_demo/app/Http/Controllers/ArticleController.php
+++ pagination_demo/app/Http/Controllers/ArticleController.php
@@ -8,39 +8,13 @@ use Illuminate\Support\Facades\Redis;
 
 class ArticleController extends Controller
 {
-    public function index(Request $request)
+    public function list(Request $request)
     {
         $per_page = 10;
         if ($request->has('per_page'))
             $per_page = $request->per_page;
 
         $articles = Article::paginate($per_page);
-
-        $json['articles'] = $articles;
-
-        return response()->json($json, 200);
-    }
-
-    public function searchArticle(Request $request)
-    {
-        $per_page = 10;
-        if ($request->has('per_page'))
-            $per_page = $request->per_page;
-
-        $articles = Article::where('title', 'LIKE', '%' . $request->title . '%')->paginate($per_page);
-
-        $json['articles'] = $articles;
-
-        return response()->json($json, 200);
-    }
-
-    public function sortArticle(Request $request)
-    {
-        $per_page = 10;
-        if ($request->has('per_page'))
-            $per_page = $request->per_page;
-
-        $articles = Article::orderBy($request->column, $request->order)->paginate($per_page);
 
         $json['articles'] = $articles;
 
